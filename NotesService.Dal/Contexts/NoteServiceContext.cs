ac63de9bc7f9eafaea736ddf325f1fd26dc664a8
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2019-06-11 14:20:09 +0300

    add repository to common add dal for noteservice

---
+++ NotesService.Dal/Contexts/NoteServiceContext.cs
@@ -0,0 +1,18 @@
+﻿using Common.Entity.NoteService;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotesService.Dal.Contexts
+{
+    public class NoteServiceContext : DbContext
+    {
+        public NoteServiceContext(DbContextOptions<NoteServiceContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        public DbSet<NoteCategory> NoteCategories { get; set; }
+    }
+}
