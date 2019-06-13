c378af4da0e48af27b04a9b4229755c7a2acd245
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2019-06-13 18:30:21 +0300

    added di

---
+++ NoteService/NoteService.DI/IoC.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NoteService.Dal.DataAccess.Implementations;
+using NoteService.Dal.DataAccess.Interfaces;
+using NotesService.Dal.Contexts;
+using NotesService.Dal.Repositories.Implementations;
+using NotesService.Dal.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteService.DI
+{
+    public static class IoC
+    {
+        public static void IoCCommonRegister(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDataAccess, DataAccess>();
+            services.AddTransient<INoteRepository, NoteRepository>();
+            services.AddTransient<INoteCategoryRepository, NoteCategoryRepository>();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<NoteServiceContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+    }
+}
