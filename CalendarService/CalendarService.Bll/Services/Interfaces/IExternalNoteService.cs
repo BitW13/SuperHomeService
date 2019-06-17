040cb3f0fc768b660d0ccd5a50890283f1c173ee
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2019-06-17 20:04:01 +0300

    calendar update

---
+++ CalendarService/CalendarService.Bll/Services/Interfaces/IExternalNoteService.cs
@@ -0,0 +1,17 @@
+﻿using Common.Entity.NoteService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarService.Bll.Services.Interfaces
+{
+    public interface IExternalNoteService
+    {
+        Task<IEnumerable<Note>> GetNotesByDay(int day, int month, int year);
+
+        Task<IEnumerable<Note>> GetNotesByMonth(int month, int year);
+
+        Task<IEnumerable<Note>> GetNotesByYear(int year);
+    }
+}
