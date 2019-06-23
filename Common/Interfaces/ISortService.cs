af30a61a71819a083086a9f8613a60fda0880ead
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2019-06-23 16:58:04 +0300

    added sortservice interfaces to common

---
+++ Common/Interfaces/ISortService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface ISortService<T>
+    {
+        Task<IEnumerable<T>> GetItemsByLastChangedAsync();
+    }
+}
